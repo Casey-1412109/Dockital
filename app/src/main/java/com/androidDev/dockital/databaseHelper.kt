8c98fa3ee5358823d588dd9cb1f6677c5f9b36b3
dhruvvishnoi136 <dhruvvishnoi136@gmail.com>
2022-12-02 01:20:53 +0530

    Firebase Connectivity Done LogIn and Sign In

--- app/src/main/java/com/androidDev/dockital/databaseHelper.kt
+++ app/src/main/java/com/androidDev/dockital/databaseHelper.kt
@@ -8,7 +8,7 @@ import com.google.firebase.database.FirebaseDatabase
 
 
 fun userStoreDataGen(name: String, emailId: String , phoneNumber: String, passWord: String): HashMap<String, Any?> {
-    var returner : HashMap<String, Any?> = hashMapOf()
+    val returner : HashMap<String, Any?> = hashMapOf()
     returner["email"] = emailId
     returner["metaMaskHash"] = "tempo"
     returner["name"] = name
@@ -16,112 +16,91 @@ fun userStoreDataGen(name: String, emailId: String , phoneNumber: String, passWo
     returner["phoneNumber"] = phoneNumber
     return returner
 }
-
 fun customToast(context: Context,toPrint : String){
     Toast.makeText(context, "$toPrint", Toast.LENGTH_LONG).show()
 }
-fun logInChecker(context: Context, dbConnect:FirebaseDatabase, emailId: String, passWord: String, navController : NavController){
+fun logInChecker(context: Context, dbConnect:FirebaseDatabase, userName: String, passWord: String, navController : NavController){
     dbConnect.getReference("/usersList").get().addOnSuccessListener{
         var usernameFetched = it.value
         if(usernameFetched != null){
-            usernameFetched = usernameFetched as ArrayList<String>
-            run breaking@{
-                usernameFetched.forEach{ mapUserChecker ->
-                    if(mapUserChecker == emailId){
-                        dbConnect.getReference("/userStore/${mapUserChecker}").get().addOnSuccessListener{
-                                mapUserFinder ->
-                            if(mapUserFinder.value != null) {
-                                var passwordFetched:String = ( mapUserFinder.value as HashMap<String, String>)["password"]!!
-                                if(passwordFetched == passWord){
-                                    navController.navigate("Home")
-                                    customToast(context, "Login Successful")
-                                }
-                                else{
-                                    customToast(context, "Enter Correct Password") // Feature
-                                }
+            usernameFetched = usernameFetched as HashMap<String, String>
+            usernameFetched.forEach{ mapUserChecker ->
+                if(mapUserChecker.value == userName){
+                    dbConnect.getReference("/userStore/${mapUserChecker.value}").get().addOnSuccessListener{
+                            mapUserFinder ->
+                        if(mapUserFinder.value != null) {
+                            var passwordFetched:String = (mapUserFinder.value as HashMap<String, String>)["password"]!!
+                            if(passwordFetched == passWord){
+                                navController.navigate("Home")
+                                Toast.makeText(context, "Login Successful", Toast.LENGTH_LONG).show()
                             }
                             else{
-                                customToast(context, "Problem Fetching Your Data. Contact Customer Care") // Feature
+                                // Feature
+                                Toast.makeText(context, "Enter Correct Password", Toast.LENGTH_LONG).show()
                             }
-                        }.addOnFailureListener{
-                            customToast(context, "Unable to Fetch Data. Try Again")
                         }
+                        else{
+                            // Feature
+                            Toast.makeText(context, "Problem Fetching Your Data. Contact Customer Care", Toast.LENGTH_LONG).show()
+                        }
+                    }.addOnFailureListener{
+                        Toast.makeText(context, "Unable to Fetch Data. Try Again", Toast.LENGTH_LONG).show()
                     }
-                    else{
-                        customToast(context, "No User Found, Please Sign In")
-                        return@forEach
-                    }
+                }
+                else{
+                    Toast.makeText(context, "No User Found, Please Sign In", Toast.LENGTH_LONG).show()
+                    return@forEach
                 }
             }
         }
         else{
-            customToast(context, "No User Found, Please Sign In")
+            Toast.makeText(context, "No User Found, Please Sign In", Toast.LENGTH_LONG).show()
         }
     }.addOnFailureListener {
-        customToast(context, "Check Your Internet Connection") // Feature
+        // Feature
+        Toast.makeText(context, "Check Your Internet Connection", Toast.LENGTH_LONG).show()
     }
 }
 
 fun signInChecker(context: Context, dbConnect:FirebaseDatabase, name: String, userName: String, emailId: String , phoneNumber: String, passWord: String, navController : NavController){
-    var runnerConfig = false
     dbConnect.getReference("/usersList").get().addOnSuccessListener{
         userListChecker ->
         if(userListChecker.value != null){
-//            run {
-                var userListFetched =  userListChecker.value as HashMap<String, String>
-                 userListFetched.forEach{
-                        aUser ->
-                    if(aUser.value == userName){
-                        customToast(context, "Username is already used")
-                        runnerConfig = true
-                        return@addOnSuccessListener
-
-                    }
+            var userListFetched =  userListChecker.value as HashMap<String, String>
+             userListFetched.forEach{
+                    aUser ->
+                if(aUser.value == userName){
+                    Toast.makeText(context, "Username is already used", Toast.LENGTH_LONG).show()
+                    return@addOnSuccessListener
                 }
-
-//            }
+            }
         }
-        else {
-            customToast(context, "Unable to check username availability")
-        }
-    }.addOnFailureListener{
-        customToast(context, "Unable to connect, Please Check Your Internet Connectivity")
-    }
-    if(!runnerConfig){
         var courier = userStoreDataGen(
             name = name,
             emailId = emailId,
             phoneNumber = phoneNumber,
             passWord = passWord
         )
-        customToast(context, "almsdklads")
         dbConnect.getReference("/userStore").child("$userName").setValue(courier).addOnSuccessListener {
             customToast(context = context, "Signed In Successfully")
-//            dbConnect.getReference("/usersList").get().addOnSuccessListener{
-//                userListFetched ->
-////                var indexCalculated = ((userListFetched.value as HashMap<String, Any>).size - 1)
-//                      #############
-//            }.addOnFailureListener{
-//                customToast(context = context, "Unable to Fetch usersList")
-//            }
             dbConnect.getReference("/usersList").push()/*.child("$indexCalculated")*/.setValue("$userName").addOnSuccessListener{
-                customToast(context = context, "User Updated In Main List")
+                Toast.makeText(context, "User Updated In Main List", Toast.LENGTH_LONG).show()
             }.addOnFailureListener{
-                customToast(context = context, "Unable To Update userList in Database")
+                Toast.makeText(context, "Unable To Update userList in Database", Toast.LENGTH_LONG).show()
             }
 
         }.addOnFailureListener {
-            customToast(context, "Unable To Create Account")
+            Toast.makeText(context, "Unable To Create Account", Toast.LENGTH_LONG).show()
             navController.navigate("Login")
 
         }
+    }.addOnFailureListener{
+        Toast.makeText(context, "Unable to connect, Please Check Your Internet Connectivity", Toast.LENGTH_LONG).show()
     }
-
 }
 
-
 fun nftPoster(context: Context, dbConnect: FirebaseDatabase, bitmap: Uri?, name:String , price: String, description : String ){
-
+    
 }
 
 
